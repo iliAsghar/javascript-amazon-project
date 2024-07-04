a5b12efe1b595c1a1d3f62811febd2db098840ef
iliAsghar <asghar.ali.z.a.r.e@gmail.com>
2024-07-04 12:52:15 +0330

    added jasmine test suites for cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,25 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || []
+export let cart;
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+//   if(!cart){
+//     cart = [
+//       {
+//         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+//         quantity: 2,
+//         deliveryOptionId: '1'
+//       },
+//       {
+//         productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+//         quantity: 1,
+//         deliveryOptionId: '2'
+//       }
+//     ]
+//     saveCartToStorage();
+//   }
+}
+loadFromStorage();
 
 function saveCartToStorage(){
   localStorage.setItem('cart',JSON.stringify(cart))
